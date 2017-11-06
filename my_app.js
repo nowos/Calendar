7cd326f0ac1d34d18fdb155afd47eee0ccd55cd8
nowos <31601190+nowos@users.noreply.github.com>
2017-11-06 11:37:40 +0100

    Update my_app.js

--- my_app.js
+++ my_app.js
@@ -1,155 +1,165 @@
-var licznik = 1;
-var click = 1 ;
-$(document).ready(function(){
- months(1);  
-});
 
-        //wyzerowanie wartości
-var to_0 = function(){
- for (var i = 1; i < 42; i++) {
-$('.month').find($('#td'+i)).text('');
-licznik = 1;
-   };
-   };
-      
-//gdy klikniemy na '<' - poprzedni miesiąc
-$('.prev_month').on('click',function(){
- to_0() ; //zeruj wartości na początek
- click = click - 1; 
- months(click);
- if(click < 1){
-    click = 1;
-    months(click);
- }
-});
+//miesiąc startowy
 
-$('.next_month').on('click',function(){
- to_0() ; //zeruj wartości na początek
- click = click +1; 
- months(click);
- if(click > 12){
-    click = 12;
-    months(12);
- }
- });
- 
-//miesiace, funckje
+var events = {};
+var click = 1;
+var x;
+var year = 2017;
+var d = new Date();
+var ar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+console.log(m);
+var m = ar[d.getMonth()];
 
-var January = function (){
- for (var i = 1; i < 32; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('January');
+// funkcja wyzerowanie wartości
+var to_0 = function () {
+   for (var i = 1; i <= 42; i++) {
+      $('.month').find($('#td' + i)).text('');
    }
+};
+//koniec wyzerowania
+//stworzenie tablicy dla miesięcy
+var month = [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December"
+];
+//koniec tablicy 
+
+
+
+//miesiąc startowy
+$('h2').text(month[m]);
+for (var i = 1; i < 32; i++) {
+   d.setDate(i);
+   document.getElementById("td" + i).innerHTML = d.getDate();
+}
+//ustaw dni
+var days = function (month, year) {
+   var firstDayOfMonth = new Date(year, month, 1);
+   x = firstDayOfMonth.getDay();
+
+   if (x === 0) {
+      x = 7;
+   }
+
+   var j = 1;
+   for (var i = x; i < daysInMonth(month + 1, year) + x; i++) {
+      document.getElementById("td" + i).innerHTML = j;
+      j = j + 1;
+   }
+   ;
+
+};
+//zwraca pierwszy dzień miesiaca
+function daysInMonth(month, year) {
+   return new Date(year, month, 0).getDate();
+}
+
+
+//gdy klikniemy na '<' - poprzedni miesiąc
+$('.prev_month').on('click', function () {
+   to_0(); //zeruj wartości na początek
+   //rok
+   if (m === 0) {
+      year = year - 1;
+      $('h1').text('Calendar' + year);
+      m = 12;
+   }
+   ;
+   //ustaw miesiąc
+   m = m - 1;
+   $('h2').text(month[ar[m]]);
+
+   //dni
+   days(m, year);
+   
+   //zapamiętaj 
+   
+});
+
+
+//gdy klikniemy na następny miesiąc
+$('.next_month').on('click', function () {
+   to_0(); //zeruj wartości na początek
+   //rok
+   if (m === 11) {
+      year = year + 1;
+      $('h1').text('Calendar' + year);
+      m = -1;
    };
+   //miesiąc
+   m = m + 1;
+   $('h2').text(month[ar[m]]);
+   //dni
+   days(m, year);
+   
+   //ładuj zadania
+   
+  for(z= 1; z <42; z ++){
+   
+	 var arrayOfTd = $('#td'+z);
+	 var tdx = arrayOfTd[0];
+	 var tdxId =  tdx.id;
+	 var moye = m + '' + year ;
+	 var fullData = moye + tdxId ; 
+	  
+	  //ustawiasz text z localStorage, jeśli 
+	 tdx.innerHtml += localStorage.getItem(fullData);  
+	  
+	  console.log(tdx);
+	  console.log($(tdx));
+ }
+ 
+ 
+});
+//wpisz tekst
+$('td').on('click', function () {
 
-var February = function(){
-for (i = 4; i < 32; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1;
-$('.h1_title').text('February');
- }
- };
- var March = function(){
-for (i = 4; i < 35; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('March');
- }
- };
-var April = function(){
-for (i = 7; i < 37; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('April');
- }
- };
-var May = function(){
-for (i = 2; i < 33; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('May');
- }
- };
-var June = function(){
-for (i = 5; i < 35; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('June');
- }
- };
-var July = function(){
-for (i = 7; i < 38; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('July');
- }
- };
-var August = function(){
-for (i = 3; i < 34; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('August');
- }
- };
-var September = function(){
-for (i = 6; i < 36; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1;
-$('.h1_title').text('September');
- }
- };
- var October = function(){
-for (i = 1; i < 32; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('October');
- }
- };
- var November = function(){
-for (i = 4; i < 34; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('November');
- }
- };
- var December = function(){
-for (i = 6; i < 37; i++) {
-$('.month').find($('#td'+i)).text(licznik);
-licznik = licznik + 1; 
-$('.h1_title').text('December');
- }
- };
- 
- var months = function(x){
-  switch(x){
-     case 1: January();
-     break;
-     case 2: February(); 
-     break;
-     case 3: March();
-     break;
-     case 4: April();
-     break;
-     case 5: May();
-     break;
-     case 6: June();
-     break;
-     case 7: July();
-     break;
-     case 8: August();
-     break;
-     case 9: September();
-     break;
-     case 10: October();
-     break;
-     case 11: November();
-     break;
-     case 12: December();
-     break;
-  }
- };
- 
- 
+   $(this).toggleClass('makeBlue');
+   var inputText = $('#input').val();
+  
+  //tworzenie zmiennej przechowującą zadanie 
+  //w zależności od wybranego pola
+   var currentData =  m + '' + year + (this).id ;
+   events[0] = inputText;
+   
+   //ustawienie teksu z inputa do wybranego pola
+   localStorage.setItem(currentData , inputText);
+   var item = localStorage.getItem(currentData);
+   
+   var displayText = ($(this).text() + ' ' + inputText );
+   $(this).text(displayText); 
+   
+   console.log(localStorage);
+   //
+   
+   
+});
 
- //odpalamy styczeń
+
+//usuń tekst
+
+
+$('#bt1').on('click', function () {
+
+   $('td').on('select', function () {
+      $(this).text('');
+   });
+});
+
+
+//ustaw zadania z local storage dla aktualnego miesiąca;
+
+//substr(string,start,length);
+
+
+
