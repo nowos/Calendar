5205239cb5eae37f0536a2ed1e3ca0995f8db067
nowos <31601190+nowos@users.noreply.github.com>
2017-11-07 13:31:20 +0100

    Update my_app.js

--- my_app.js
+++ my_app.js
@@ -1,22 +1,9 @@
-
-//miesiąc startowy
-
 var events = {};
-var click = 1;
-var x;
-var year = 2017;
 var d = new Date();
-var ar = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-console.log(m);
-var m = ar[d.getMonth()];
+var year = d.getFullYear();
+console.log(year);
+var m = d.getMonth();
 
-// funkcja wyzerowanie wartości
-var clearTable = function () {
-   for (var i = 1; i <= 42; i++) {
-      $('.month').find($('#td' + i)).text('');
-   }
-};
-//koniec wyzerowania
 //stworzenie tablicy dla miesięcy
 var month = [
    "January",
@@ -32,36 +19,14 @@ var month = [
    "November",
    "December"
 ];
-//koniec tablicy 
 
-
-
-//miesiąc startowy
-$('h2').text(month[m]);
-for (var i = 1; i < 32; i++) {
-   d.setDate(i);
-   document.getElementById("td" + i).innerHTML = d.getDate();
-
-   //ładuj zadania
-
-   for (z = 1; z < 42; z++) {
-
-      var arrayOfTd = $('#td' + z);
-      var tdX = arrayOfTd[0];
-      var tdXId = tdX.id;
-      var monthAndYear = m + '' + year;
-      var fullData = monthAndYear + tdXId;
-
-      //ustawiasz text z localStorage, jeśli 
-      console.log(localStorage.getItem(fullData));
-      if (localStorage.getItem(fullData))
-      {
-         $(tdX).append(' ' + localStorage.getItem(fullData));
-      } else {
-      }
+// funkcja wyzerowanie wartości
+var clearTable = function () {
+   for (var i = 1; i <= 42; i++) {
+      $('.month').find($('#td' + i)).text('');
    }
+};
 
-}
 //ustaw dni
 var days = function (month, year) {
    var firstDayOfMonth = new Date(year, month, 1);
@@ -70,134 +35,125 @@ var days = function (month, year) {
    if (x === 0) {
       x = 7;
    }
-
    var j = 1;
+
    for (var i = x; i < daysInMonth(month + 1, year) + x; i++) {
       document.getElementById("td" + i).innerHTML = j;
       j = j + 1;
    }
-   ;
-
-
-
 };
+
+//rysuj miesiąc z zadaniami
+var fillTable = function (m, year) {
+   $('h2').text(month[m]);
+   $('h1').text('Calendar ' + year);
+
+   days(m, year);
+
+   //ładuj zadania
+   for (z = 1; z < 42; z++) {
+
+      var arrayOfTd = $('#td' + z);
+      var tdX = arrayOfTd[0];
+      var tdXId = tdX.id;
+      var monthAndYear = m + '' + year;
+      var fullData = monthAndYear + tdXId;
+
+      //ustawiasz text z localStorage
+      if (localStorage.getItem(fullData)) {
+         $(tdX).append(' ' + localStorage.getItem(fullData));
+      }
+   }
+};
+
 //zwraca pierwszy dzień miesiaca
 function daysInMonth(month, year) {
    return new Date(year, month, 0).getDate();
 }
 
-
 //gdy klikniemy na '<' - poprzedni miesiąc
-$('.prev_month').on('click', function () {
-   clearTable(); //zeruj wartości na początek
-   //rok
-   if (m === 0) {
-      year = year - 1;
-      $('h1').text('Calendar' + year);
-      m = 12;
-   }
-   ;
-   //ustaw miesiąc
-   m = m - 1;
-   $('h2').text(month[ar[m]]);
-
-   //dni
-   days(m, year);
-
-   //ładuj zadania
-
-   for (z = 1; z < 42; z++) {
-
-      var arrayOfTd = $('#td' + z);
-      var tdX = arrayOfTd[0];
-      var tdXId = tdX.id;
-      var monthAndYear = m + '' + year;
-      var fullData = monthAndYear + tdXId;
-
-      //ustawiasz text z localStorage, jeśli 
-      console.log(localStorage.getItem(fullData));
-      if (localStorage.getItem(fullData))
-      {
-         $(tdX).append(' ' + localStorage.getItem(fullData));
-      } else {
+var prevMonthButtonListener = function () {
+   $('.prev_month').on('click', function () {
+      clearTable(); //zeruj wartości na początek
+      //rok
+      if (m === 0) {
+         year = year - 1;
+         m = 12;
       }
-   }
+      //ustaw miesiąc
+      m = m - 1;
 
-});
+      //wypełnij tabelę
+      fillTable(m, year);
 
+   });
+};
 
 //gdy klikniemy na następny miesiąc
-$('.next_month').on('click', function () {
-   clearTable(); //zeruj wartości na początek
-   //rok
-   if (m === 11) {
-      year = year + 1;
-      $('h1').text('Calendar' + year);
-      m = -1;
-   }
-   ;
-   //miesiąc
-   m = m + 1;
-   $('h2').text(month[ar[m]]);
-   //dni
-   days(m, year);
+var nextMonthButtonListener = function () {
 
-   //ładuj zadania
-
-   for (z = 1; z < 42; z++) {
-
-      var arrayOfTd = $('#td' + z);
-      var tdX = arrayOfTd[0];
-      var tdXId = tdX.id;
-      var monthAndYear = m + '' + year;
-      var fullData = monthAndYear + tdXId;
-
-      //ustawiasz text z localStorage, jeśli 
-      console.log(localStorage.getItem(fullData));
-      if (localStorage.getItem(fullData))
-      {
-         $(tdX).append(' ' + localStorage.getItem(fullData));
-      } else {
+   $('.next_month').on('click', function () {
+      clearTable(); //zeruj wartości na początek
+      //rok
+      if (m === 11) {
+         year = year + 1;
+         m = -1;
       }
-   }
-   //koniec funkcji 'następny miesiąc' 
+      //miesiąc
+      m = m + 1;
+      //wypełnij tabelę
+      fillTable(m, year);
 
-});
+   });
+};
+
 //wpisz zadanie
-$('td').on('click', function () {
+var tdClickListener = function () {
+   $('td').on('click', function () {
+      //jeśli komórka przechowuje dzień
+      if ($(this).text().trim().length > 0) {
 
-   var inputText = $('#input').val();
+         var inputText = $('#input').val();
 
-   //tworzenie zmiennej przechowującą zadanie 
-   //w zależności od wybranego pola
-   var currentData = m + '' + year + (this).id;
-   events[0] = inputText;
+         //tworzenie zmiennej przechowującą zadanie 
+         //w zależności od wybranego pola
+         var currentData = m + '' + year + (this).id;
+         events[0] = inputText;
 
-   //ustawienie teksu z inputa do wybranego pola
-   localStorage.setItem(currentData, inputText);
-   var item = localStorage.getItem(currentData);
+         var item = localStorage.getItem(currentData);
 
-   var displayText = ($(this).text() + ' ' + inputText);
-   $(this).text(displayText);
-
-
-});
-
+         if (item) {
+            alert(item);
+         } else {
+            //ustawienie teksu z inputa do wybranego pola i zapisanie w localstorage
+            localStorage.setItem(currentData, inputText);
+            var displayText = ($(this).text() + ' ' + inputText);
+            $(this).text(displayText);
+         }
+      }
+   });
+};
 
 //usuń zadania
+var clearButtonListener = function () {
+   $('.btClear').on('click', function () {
+      localStorage.clear();
+      location.reload();
+   });
+};
 
+//miesiąc startowy
+var initCalendar = function () {
+   fillTable(m, year);
+};
 
-$('#bt1').on('click', function () {
+var main = function () {
+   initCalendar();
+   nextMonthButtonListener();
+   prevMonthButtonListener();
+   clearButtonListener();
+   tdClickListener();
+};
 
-   localStorage.clear();
-   location.reload()
-
-});
-
-
-//ustaw zadania z local storage dla aktualnego miesiąca;
-
-//substr(string,start,length);
-
-
+$(document).ready(main);
 
